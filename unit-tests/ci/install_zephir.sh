1f19c93f52b1fbdceb4dafcf194bd287938c7cf6
wapmorgan <wapmorgan@gmail.com>
2014-09-15 19:27:19 +0400

    Update install_zephir.sh

--- unit-tests/ci/install_zephir.sh
+++ unit-tests/ci/install_zephir.sh
@@ -2,8 +2,10 @@
 
 # Build and install Zephir https://github.com/phalcon/zephir
 d=/usr/local/src/zephir
-sudo git clone https://github.com/phalcon/zephir.git $d
+sudo mkdir $d
+sudo chown $USER $d
+git clone https://github.com/phalcon/zephir.git $d
 cd $d
-sudo git checkout ${ZEPHIR_VER}
+git checkout ${ZEPHIR_VER}
 sudo ./install
 sudo ln -s /usr/local/src/zephir/bin/zephir /usr/local/bin
